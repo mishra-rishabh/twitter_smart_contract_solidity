dc065b752fc324761e5fa9d499a4bc44ae726e25
mishra-rishabh <rishabh.ramg@gmail.com>
2023-09-26 23:23:34 +0530

    adding Profile contract

---
+++ Profile.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract Profile {
+    struct UserProfile {
+        string displayName;
+        string bio;
+    }
+
+    mapping(address => UserProfile) profiles;
+
+    function setProfile(string memory _displayName, string memory _bio) public {
+        profiles[msg.sender] = UserProfile(_displayName, _bio);
+    }
+
+    function getProfile(address _user) public view returns (UserProfile memory) {
+        return profiles[_user];
+    }
+}
